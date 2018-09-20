234b5aa76288261ff5c1f9ff0621ebf54f05e43e
Simon <le6monstre@hotmail.com>
2018-09-20 13:32:25 -0400

    Ajout du changment d'orientation du sprite du joueur. Ajout des mouvements du reaper.

---
+++ Assets/EntityControlableController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityControlableController : MonoBehaviour {
+    [SerializeField] protected float speed;
+    protected Animator animator;
+    protected SpriteRenderer sprite;
+
+    protected Transform root;
+
+    protected float horizontalMovement = 0.0f;
+
+    protected float HorizontalMovement
+    {
+        get
+        {
+            return horizontalMovement;
+        }
+        set
+        {
+            horizontalMovement = value;
+            if (horizontalMovement > 0)
+                sprite.flipX = false;
+            else if (horizontalMovement < 0)
+                sprite.flipX = true;
+        }
+    }
+
+    private void Awake()
+    {
+        root = gameObject.transform.parent;
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        HorizontalMovement = Input.GetAxis("Horizontal") * speed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+    }
+
+    private void FixedUpdate()
+    {
+        root.Translate(horizontalMovement * Time.deltaTime, 0, 0);
+    }
+}
