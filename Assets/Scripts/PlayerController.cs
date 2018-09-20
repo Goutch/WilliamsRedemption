21b42ca01642ea86af3dbf073dc7846db5a95b93
Simon <le6monstre@hotmail.com>
2018-09-20 12:46:06 -0400

    Création personnage, déplacement gauche/droite. (Animation Idle/Walk)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private GameObject william;
+    [SerializeField] private GameObject reaper;
+
+    private void Start()
+    {
+        OnLightEnter();
+    }
+
+    public void OnLightEnter()
+    {
+        william.SetActive(true);
+        reaper.SetActive(false);
+    }
+
+    public void OnLightLeft()
+    {
+        william.SetActive(false);
+        reaper.SetActive(true);
+    }
+}
---
+++ Assets/WilliamController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilliamController : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    private Animator animator;
+
+    private Transform root;
+
+    private float horizontalMovement = 0.0f;
+
+    private void Awake()
+    {
+        root = gameObject.transform.parent;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update ()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * speed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+    }
+
+    private void FixedUpdate()
+    {
+        root.Translate(horizontalMovement * Time.deltaTime, 0,0);
+    }
+}
