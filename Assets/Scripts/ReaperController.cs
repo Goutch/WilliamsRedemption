8007e103049a1d8bfc40f1baa721394f1e4780d9
Simon <le6monstre@hotmail.com>
2018-09-21 14:13:30 -0400

    Ajout du dash (Animation incluse). Reorganisation des classes du joueur

--- Assets/Scripts/ReaperController.cs
+++ Assets/Scripts/ReaperController.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReaperController : EntityControlableController {
-
+public class ReaperController : EntityControlableController
+{
+    public override void UseCapacity1(IPlayerData data)
+    {
+    }
+    public override bool Capacity1Usable(IPlayerDataReadOnly data)
+    {
+        return true;
+    }
 }
--- Assets/Scripts/WilliamController.cs
+++ Assets/Scripts/WilliamController.cs
@@ -4,4 +4,42 @@ using UnityEngine;
 
 public class WilliamController : EntityControlableController
 {
+    [SerializeField] private float dashForce;
+    [SerializeField] private float durationOfDash;
+
+    protected bool capacityUsedOnceInAir = false;
+
+    public override void UseCapacity1(IPlayerData data)
+    {
+        StartCoroutine(Dash(data));
+        capacityUsedOnceInAir = true;
+    }
+
+    public override bool Capacity1Usable(IPlayerDataReadOnly data)
+    {
+        if (data.IsOnGround)
+            capacityUsedOnceInAir = false;
+
+        if (capacityUsedOnceInAir)
+            return false;
+        else
+            return true;
+    }
+
+    IEnumerator Dash(IPlayerData data)
+    {
+        data.IsDashing = true;
+        float numbOfTimePassed = 0;
+
+        while (numbOfTimePassed < durationOfDash)
+        {
+            numbOfTimePassed += Time.deltaTime;
+            data.RigidBody.velocity = new Vector2(Vector2.right.x * (sprite.flipX ? dashForce * -1 : dashForce), 0.0f);
+
+            yield return null;
+        }
+        data.RigidBody.velocity = new Vector2(0, data.RigidBody.velocity.y);
+
+        data.IsDashing = false;
+    }
 }
