2889b927683ad7bb43c4b9bac274070675307d87
Mathieu Bédard <mthb54@live.ca>
2018-09-24 11:37:24 -0400

    Portes
    Contient les portes , les switch et les tripwire pour les portes.

---
+++ Assets/Scripts/DoorSwitchController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitchController : MonoBehaviour
+{
+
+	[SerializeField] private GameObject door;
+	private DoorScript _doorScript;
+
+
+	void Awake()
+	{
+		_doorScript = door.GetComponent<DoorScript>();
+	}
+	
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			_doorScript.Open();
+		}
+	}
+
+}
