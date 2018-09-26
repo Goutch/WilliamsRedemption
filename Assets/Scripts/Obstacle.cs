27ecdf052298a4d3c9adfd10397321d7838e9225
Gabriel Bouchard <gaboutch@outlook.com>
2018-09-26 14:23:10 -0400

    obstacles

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Obstacle:MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.tag == R.E.Tag.Player.ToString())
+            {
+                other.collider.Root().GetComponent<PlayerController>().DamagePlayer();
+            }
+        }
+    }
+}
