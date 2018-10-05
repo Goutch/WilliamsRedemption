563dbe11feb18d4dd108d88ee503850632742586
Gabriel Bouchard <gaboutch@outlook.com>
2018-10-05 08:05:33 -0400

    debut score

--- Assets/Scripts/Enemies/Health.cs
+++ Assets/Scripts/Enemies/Health.cs
@@ -7,6 +7,7 @@ public delegate void HealthEventHandler();
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth;
+	private bool isKilledByPlayer = true;
     public int MaxHealth => maxHealth;
 	private int healthPoints;
 	public event HealthEventHandler OnDeath;
@@ -38,6 +39,7 @@ public class Health : MonoBehaviour
 
 	public void Kill()
 	{
+		isKilledByPlayer = false;
 		HealthPoints = 0;
 	}
 }
