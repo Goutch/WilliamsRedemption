a878e2936a895e5823dd60df0359a502fc66ef58
Gabriel Bouchard <gaboutch@outlook.com>
2018-10-22 10:35:26 -0400

    commit

---
+++ Assets/Scripts/UI/CollectablesUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Game.UI
+{
+    public class CollectablesUI:MonoBehaviour
+    {
+        [SerializeField] private GameObject collectableUIElementPrefab;
+        [SerializeField] private Transform gridLayout;
+        private void AddCollectable()
+        {
+            Instantiate(collectableUIElementPrefab,gridLayout.transform);
+        }
+    }
+}
