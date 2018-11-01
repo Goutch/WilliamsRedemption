334cdcf0b0fee16c609b40f361ebdc0e99089932
mthb54 <42650980+mthb54@users.noreply.github.com>
2018-10-31 21:31:41 -0400

    MInor scene fix

--- Assets/Scripts/Game/Puzzle/Light/LightStimulu.cs
+++ Assets/Scripts/Game/Puzzle/Light/LightStimulu.cs
@@ -17,6 +17,7 @@ namespace Game.Puzzle.Light
             movingLightObstaclesInRange = new List<MovingLightObstacle>();
 
             meshLight = GetComponent<MeshLight>();
+            int h = 0;
         }
 
         private void Update()
