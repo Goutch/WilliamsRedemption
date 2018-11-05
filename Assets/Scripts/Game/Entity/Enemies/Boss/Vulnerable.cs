8c77da7ef30c1675a8167cd322edadc8ea24cff7
Simon <le6monstre@hotmail.com>
2018-11-05 11:25:16 -0500

    Finition du code de Zek'gor

--- Assets/Scripts/Game/Entity/Enemies/Boss/Vulnerable.cs
+++ Assets/Scripts/Game/Entity/Enemies/Boss/Vulnerable.cs
@@ -1,5 +1,6 @@
 ﻿using Harmony;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Entity.Enemies.Boss
@@ -9,21 +10,16 @@ namespace Game.Entity.Enemies.Boss
         [Tooltip("Use Trigger '" + Values.AnimationParameters.Edgar.Vulnerable + "' ")]
         [SerializeField] private Animator animator;
         [SerializeField] private float duration;
-        [SerializeField] private bool disableDamage = false;
         [SerializeField] private float delayBeforeDoingDamageToPlayerAfterLeavingState;
-        private HitStimulus[] hitStimuli;
+        [SerializeField] private Collider2D attackCollider;
 
         private float timeEntered;
 
-        private void Start()
-        {
-            hitStimuli = this.GetComponentsInObject<HitStimulus>();
-        }
         public override void Finish()
         {
             base.Finish();
 
-            if(disableDamage)
+            if(delayBeforeDoingDamageToPlayerAfterLeavingState != 0)
                 StartCoroutine(enableStimulus());
         }
 
@@ -41,10 +37,11 @@ namespace Game.Entity.Enemies.Boss
         {
             base.Enter();
 
-            if(disableDamage)
+            if(delayBeforeDoingDamageToPlayerAfterLeavingState != 0 && attackCollider != null)
             {
-                foreach (HitStimulus hitStimulus in hitStimuli)
-                    hitStimulus.Enabled = false;
+                attackCollider.enabled = false;
+
+                StopAllCoroutines();
             }
 
             timeEntered = Time.time;
@@ -55,8 +52,7 @@ namespace Game.Entity.Enemies.Boss
         {
             yield return new WaitForSeconds(delayBeforeDoingDamageToPlayerAfterLeavingState);
 
-            foreach (HitStimulus hitStimulus in hitStimuli)
-                hitStimulus.Enabled = true;
+            attackCollider.enabled = true;
         }
     }
 }
