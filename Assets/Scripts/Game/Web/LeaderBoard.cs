f67aaaf70cdbf34dccbfde99981258bf008742c9
FlyJonat <flyjonat@live.fr>
2018-11-09 09:14:12 -0500

    first code to test

---
+++ Assets/Scripts/Game/Web/LeaderBoard.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Game.Web
+{
+    public class LeaderBoard : MonoBehaviour
+    {
+        public bool InsertDataToDatabase(string name, int score, int time)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("name",name);
+            form.AddField("score_value",score);
+            form.AddField("time", time);
+
+            using (UnityWebRequest www = UnityWebRequest.Post("http://35.188.160.44/api/insert", form))
+            {
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
