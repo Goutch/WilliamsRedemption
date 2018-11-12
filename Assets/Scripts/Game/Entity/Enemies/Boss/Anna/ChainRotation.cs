2b7878b5d98dda0f93b3f6f83f5f96daf691e295
Simon <le6monstre@hotmail.com>
2018-11-12 14:47:10 -0500

    P2 rotation

---
+++ Assets/Scripts/Game/Entity/Enemies/Boss/Anna/ChainRotation.cs
@@ -0,0 +1,54 @@
+﻿using Game.Entity.Enemies.Boss;
+using Game.Puzzle.Light;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Game.Entity.Enemies.Boss.Anna
+{
+    class ChainRotation : SequentialLoopPhase
+    {
+        [SerializeField] private GameObject chain;
+        [SerializeField] private float rotationSpeed;
+        [SerializeField] private Transform teleportationPoint;
+        [SerializeField] private new MeshLight light;
+
+        private Animator animator;
+        private SpawnedEnemyManager enemyManager;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            enemyManager = GetComponent<SpawnedEnemyManager>();
+        }
+
+        public override bool CanEnter()
+        {
+            return true;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            animator.SetTrigger(Values.AnimationParameters.Anna.Special);
+            enemyManager.Clear();
+
+            chain.SetActive(true);
+
+            light.Close();
+
+            transform.position = teleportationPoint.transform.position;
+        }
+
+        protected override void Idle()
+        {
+            base.Idle();
+
+            chain.transform.Rotate(-Vector3.forward, rotationSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Entity/Enemies/Boss/Anna/SpotSpawnEnemy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Game.Entity.Enemies.Boss.Anna
+{
+    class SpotSpawnEnemy : Capacity
+    {
+        [SerializeField] private GameObject enemyToSpawn;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private float spawnDelay;
+
+        private SpawnedEnemyManager enemyManager;
+
+        private float timeEnemyDied;
+
+        private void Awake()
+        {
+            enemyManager = GetComponent<SpawnedEnemyManager>();
+        }
+
+        private void OnEnable()
+        {
+            enemyManager.OnEnnemyDied += EnemyManager_OnEnnemyDied;
+        }
+
+        private void OnDisable()
+        {
+            enemyManager.OnEnnemyDied -= EnemyManager_OnEnnemyDied;
+        }
+
+        private void EnemyManager_OnEnnemyDied(GameObject gameObject)
+        {
+            timeEnemyDied = Time.time;
+        }
+
+        public override void Act()
+        {
+
+        }
+
+        public override bool CanEnter()
+        {
+            return enemyManager.CanSpawnEnemy() && Time.time - timeEnemyDied > spawnDelay;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            enemyManager.SpawnEnemy(enemyToSpawn, spawnPoint.position, Quaternion.identity);
+        }
+    }
+}
