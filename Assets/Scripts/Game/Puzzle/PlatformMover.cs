84ce1bc01500d5317fb6374e8002eef2e9f17061
mthb54 <42650980+mthb54@users.noreply.github.com>
2018-11-12 21:52:41 -0500

    I tried

--- Assets/Scripts/Game/Puzzle/PlatformMover.cs
+++ Assets/Scripts/Game/Puzzle/PlatformMover.cs
@@ -1,5 +1,9 @@
-﻿using Game.Entity.Player;
+﻿using System.Collections.Generic;
+using System.Timers;
+using Game.Entity.Player;
+using Harmony;
 using UnityEngine;
+using UnityScript.Steps;
 
 namespace Game.Puzzle
 {
@@ -50,26 +54,25 @@ namespace Game.Puzzle
         private float initialPositionY;
         private float quadraticX;
         private Vector2 lastRbPosition;
-        private Rigidbody2D rb;
         private Vector2 horizontalDirection;
         private Vector2 verticalDirection;
-        private float quadraticFucntion;
+        private float quadraticFunction;
         private float positionX;
-        
-        private Vector2 heightOffset;
+        private HashSet<Transform> colliders;
+        private Vector2 curve;
+        private Vector3 heightOffset;
 
 
         // Use this for initialization
         private void Awake()
         {
-            
-            rb = GetComponent<Rigidbody2D>();
-            initialPositionX = rb.position.x;
-            initialPositionY = rb.position.y;
+            initialPositionX = transform.position.x;
+            initialPositionY = transform.position.y;
             positionX = initialPositionX;
             quadraticX = 0;
-            
-            lastRbPosition = rb.position;
+            colliders = new HashSet<Transform>();
+            lastRbPosition = transform.position;
+            heightOffset = new Vector3(0, GetComponent<BoxCollider2D>().size.y,0);
         }
 
         // Update is called once per frame
@@ -82,44 +85,85 @@ namespace Game.Puzzle
 
         private void FixedUpdate()
         {
-            
+            Vector3 temp = transform.position;
             if (!isUsingQuadraticCurve)
             {
-                rb.velocity = new Vector2(horizontalDirection.x * HorizontalSpeed, verticalDirection.y * VerticalSpeed);
+                
+                Vector3 t= new Vector3(horizontalDirection.x * HorizontalSpeed, verticalDirection.y * VerticalSpeed)*Time.fixedDeltaTime;
+                transform.Translate(t);
+                
+                if (colliders.Count >0)
+                {
+                    foreach (var collider in colliders)
+                    {
+                        collider.Translate(t);
+                    }
+                }
             }
             else
             {
                 useQuadraticCurve();
+                
+                if (colliders.Count >0)
+                {
+                    foreach (var collider in colliders)
+                    {
+                        
+                        collider.Translate(curve);
+                    }
+                }
             }
+
+            
 
             
         }
 
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag(Values.Tags.Player))
+            {
+                if (!colliders.Contains(other.transform))
+                {
+                    colliders.Add(other.transform);
+                }
+            }
+        }
+        
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (colliders.Contains(other.transform))
+            {
+                colliders.Remove(other.transform);
+            }
+        }
+
         private void OnCollisionStay2D(Collision2D other) //bouge ce qui touche a la plateforme (Donne y le meme mouvement) Liste des colliders pis bouge tout ce qui colide avec dans le fixedUpdate.
         {
-            if (other.collider.CompareTag(Values.Tags.Player))
-            {
-                PlayerController player = other.gameObject.GetComponent<PlayerController>();
-
-                Vector3 offset = player.transform.position - transform.position;
-                
-
-                if (!isUsingQuadraticCurve)
-                {
-                   // Vector2 v = new Vector2(horizontalDirection.x*HorizontalSpeed*Time.fixedDeltaTime,VerticalSpeed*verticalDirection.y*Time.fixedDeltaTime);
-                    player.transform.position = transform.position + offset;     
-                    
-                    
-                }
-                else
-                {
-                    float xDistance = rb.position.x - lastRbPosition.x;
-                    float yDistance = rb.position.y - lastRbPosition.y;
-                    Vector2 v = new Vector2((xDistance),(0));
-                   
-                    player.kRigidBody.Velocity +=v/Time.fixedDeltaTime;
-                }             
-            }        
+//            if (other.collider.CompareTag(Values.Tags.Player))
+//            {
+//                PlayerController player = other.gameObject.GetComponent<PlayerController>();
+//
+//                Vector3 offset = player.transform.position - transform.position;
+//                
+//
+//                if (!isUsingQuadraticCurve)
+//                {
+//                   // Vector2 v = new Vector2(horizontalDirection.x*HorizontalSpeed*Time.fixedDeltaTime,VerticalSpeed*verticalDirection.y*Time.fixedDeltaTime);
+//                    player.transform.position = transform.position + offset;     
+//                    
+//                    
+//                }
+//                else
+//                {
+//                    float xDistance = rb.position.x - lastRbPosition.x;
+//                    float yDistance = rb.position.y - lastRbPosition.y;
+//                    Vector2 v = new Vector2((xDistance),(0));
+//                   
+//                    player.kRigidBody.Velocity +=v/Time.fixedDeltaTime;
+//                }             
+//            }        
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -132,10 +176,6 @@ namespace Game.Puzzle
             
         }
 
-        private void OnCollisionExit2D(Collision2D other)
-        {
-            
-        }
 
         private void CheckHorizontalDirection()
         {
@@ -191,13 +231,19 @@ namespace Game.Puzzle
 
         private void useQuadraticCurve()
         {
-            lastRbPosition = rb.position;
-            quadraticX = rb.position.x - initialPositionX;
-            positionX += HorizontalSpeed * horizontalDirection.x *Time.fixedDeltaTime;
-            quadraticFucntion = (quadraticA * (quadraticX * quadraticX) + quadraticB * (quadraticX) + quadraticC);
-            Vector2 curve = new Vector2(positionX, quadraticFucntion + initialPositionY);
-            rb.MovePosition(curve);
-            
+//            lastRbPosition = rb.position;
+//            quadraticX = rb.position.x - initialPositionX;
+//            positionX += HorizontalSpeed * horizontalDirection.x *Time.fixedDeltaTime;
+//            quadraticFucntion = (quadraticA * (quadraticX * quadraticX) + quadraticB * (quadraticX) + quadraticC);
+//            Vector2 curve = new Vector2(positionX, quadraticFucntion + initialPositionY);
+//            rb.MovePosition(curve);
+
+            lastRbPosition = transform.position;
+            quadraticX = transform.position.x - initialPositionX;
+            positionX += HorizontalSpeed * horizontalDirection.x * Time.fixedDeltaTime;
+            quadraticFunction = (quadraticA * (quadraticX * quadraticX) + quadraticB * (quadraticX) + quadraticC);
+            curve = new Vector2(positionX,quadraticFunction + initialPositionY);
+            transform.Translate(curve - lastRbPosition);
         }
     }
 }
