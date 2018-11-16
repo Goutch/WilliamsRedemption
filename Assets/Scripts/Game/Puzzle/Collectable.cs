9985d6c2820bbcbdc0fe33eab40697af1a08bc31
FlyJonat <flyjonat@live.fr>
2018-11-16 10:44:25 -0500

    add collectable sound

--- Assets/Scripts/Game/Puzzle/Collectable.cs
+++ Assets/Scripts/Game/Puzzle/Collectable.cs
@@ -6,14 +6,26 @@ namespace Game.Puzzle
     public class Collectable : MonoBehaviour
     {
         [SerializeField] private int scoreValue;
+        [SerializeField] private AudioClip collectableSound;
+        [SerializeField] private GameObject soundToPlayPrefab;
+        
+        private GameObject soundToPlay;
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag(Values.Tags.Player))
             {
+                UseSound();
                 GameObject.FindGameObjectWithTag(Values.Tags.GameController).GetComponent<GameController>().AddCollectable(scoreValue);
                 Destroy(this.gameObject);
             }
+        }
+        
+        private void UseSound()
+        {
+            soundToPlay=Instantiate(soundToPlayPrefab,this.transform.position,Quaternion.identity);
+            soundToPlay.GetComponent<AudioManagerSpecificSounds>().Init(collectableSound, false, this.gameObject);
+            soundToPlay.GetComponent<AudioManagerSpecificSounds>().PlaySound();
         }
     }
 }
