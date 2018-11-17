b88b2b4276e8ef584aa5af72a94ec27f82e20e38
Mathieu Bédard <mthb54@live.ca>
2018-11-17 11:20:18 -0500

    Update LightWheel.cs

--- Assets/Scripts/Game/Puzzle/LightWheel.cs
+++ Assets/Scripts/Game/Puzzle/LightWheel.cs
@@ -6,55 +6,26 @@ using UnityEngine;
 
 public class LightWheel : MonoBehaviour
 {
-
-	[SerializeField] private CircleLight[] lights = new CircleLight[4];
-	[SerializeField] private List<CircleLight> ligghts = new List<CircleLight>(4);
+	[SerializeField] private List<CircleLight> lights = new List<CircleLight>(4);
 	[SerializeField] private float rotatingSpeed;
-	private float angleR;
-	private BoxCollider2D box;
 
 	// Use this for initialization
 	private void Awake()
 	{
-		box = GetComponent<BoxCollider2D>();
-		angleR = 0;
 	}
 
-	private void Start()
-	{
-		/*float angle = 0;
-		for (int i = 0; i < lights.Length;i++)
-		{
-			lights[i].FaceAngle = angle;
-			angle += 90;
-			if (i == 1)
-			{
-				lights[i].transform.position = new Vector3(box.transform.position.x,box.transform.position.y+box.size.y/2);
-			}
-			else if (i == 3)
-			{
-				lights[i].transform.position = new Vector3(box.transform.position.x,box.transform.position.y -box.size.y/2);
-			}
-			else if(i ==0)
-			{
-				lights[i].transform.position = new Vector3(box.transform.position.x+box.size.x/2,box.transform.position.y);
-			}
-			else
-			{
-				lights[i].transform.position = new Vector3(box.transform.position.x-box.size.x/2,box.transform.position.y);
-			}
-				
-		}*/
-	}
+
 	
 	// Update is called once per frame
 	private void Update ()
 	{
-		angleR += rotatingSpeed * Time.deltaTime;
 		RotateWheel();
 	}
 	private void RotateWheel()
-	{			
-		transform.Rotate(0,0,angleR);
+	{	
+		foreach (var l in lights)
+		{
+			l.FaceAngle += rotatingSpeed*Time.deltaTime;
+		}
 	}
 }
