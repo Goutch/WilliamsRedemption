5deddf97c7ab81ecbe05da1efe4f37b3b2e025d3
FlyJonat <flyjonat@live.fr>
2018-11-19 11:20:09 -0500

    unfinished music trigger

---
+++ Assets/Scripts/Game/Audio/MusicTriggers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Game.Audio
+{
+    public class MusicTriggers : MonoBehaviour
+    {
+        private AudioManagerBackgroundSound audioManager;
+        //Valeur temporaire
+        private AudioClip levelMusic;
+        private bool isMusicPlaying = false;
+        
+        private void Awake()
+        {
+            audioManager = GameObject.FindGameObjectWithTag(Values.Tags.MainCamera)
+                .GetComponent<AudioManagerBackgroundSound>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!isMusicPlaying)
+            {
+                audioManager.Init(levelMusic);
+                audioManager.PlaySound();
+            }
+            else
+            {
+                audioManager.StopSound();
+            }
+        }
+    }
+}
