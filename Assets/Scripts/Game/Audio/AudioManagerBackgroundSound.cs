7debaa44e6c838acb58610d73eb00410f3dbfba8
FlyJonat <flyjonat@live.fr>
2018-11-19 11:06:06 -0500

    add code for boss fight music

--- Assets/Scripts/Game/Audio/AudioManagerBackgroundSound.cs
+++ Assets/Scripts/Game/Audio/AudioManagerBackgroundSound.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using Game.Entity;
+using Game.Entity.Player;
 using UnityEngine;
 
 public class AudioManagerBackgroundSound : MonoBehaviour
 {
 	private AudioSource source;
+	private AudioClip clip;
+	private bool shouldMusicBePlaying;
 
 	private void Awake()
 	{
 		source = GetComponent<AudioSource>();
+		shouldMusicBePlaying = false;
 	}
 
-	public void PlaySound(AudioClip clip)
+	private void Start()
+	{
+		PlayerController.instance.GetComponent<Health>().OnDeath += OnPlayerDie;
+	}
+	
+	public void Init(AudioClip clip)
+	{
+		this.clip = clip;
+		shouldMusicBePlaying = true;
+	}
+
+	private void Update()
+	{
+		if (!source.isPlaying && shouldMusicBePlaying)
+		{
+			PlaySound();
+		}
+	}
+
+	private void OnPlayerDie(GameObject gameObject)
+	{
+		StopSound();
+	}
+	
+	public void PlaySound()
 	{
 		source.PlayOneShot(clip,1F);
+	}
+
+	public void StopSound()
+	{
+		source.Stop();
+		shouldMusicBePlaying = false;
 	}
 }
