fb18e50e4e45decf56f1c30f7e9bd3e3f6732bcd
Mathieu Bédard <mthb54@live.ca>
2018-11-23 14:39:36 -0500

    Code Cleaning

--- Assets/Scripts/Game/Puzzle/LightWheel.cs
+++ Assets/Scripts/Game/Puzzle/LightWheel.cs
@@ -9,18 +9,12 @@ public class LightWheel : MonoBehaviour
 	[SerializeField] private List<CircleLight> lights = new List<CircleLight>(4);
 	[SerializeField] private float rotatingSpeed;
 
-	// Use this for initialization
-	private void Awake()
-	{
-	}
-
-
 	
-	// Update is called once per frame
 	private void Update ()
 	{
 		RotateWheel();
 	}
+	
 	private void RotateWheel()
 	{	
 		foreach (var l in lights)
