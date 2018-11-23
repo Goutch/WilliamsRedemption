b398e7d72cef1541f80ece085de3fdfe3291eafb
FlyJonat <flyjonat@live.fr>
2018-11-23 12:10:08 -0500

    delete useless values

--- Assets/Scripts/Game/Values/GameObject.cs
+++ Assets/Scripts/Game/Values/GameObject.cs
@@ -9,18 +9,5 @@
         public const string Anna = "Anna";
         public const string ZekGor = "ZekGor";
         public const string Death = "Death";
-        public const string TimeText = "GameController/Canvas/TimeText";
-        public const string ScoreText = "GameController/Canvas/ScoreText";
-        public const string PanelPause = "GameController/Canvas/PanelPause";
-        public const string ButtonRestartGame = "GameController/Canvas/PanelPause/ButtonRestartGame";
-        public const string TextPause = "GameController/Canvas/PanelPause/Text";
-        public const string NameFieldText = "GameController/Canvas/PanelPause/NameField/Text";
-        public const string NameField = "GameController/Canvas/PanelPause/NameField";
-        public const string ButtonTestInsertData = "GameController/Canvas/PanelPause/ButtonTestInsertData";
-        public const string Menu = "Scenes/Menu";
-        public const string Level1 = "Level1";
-        public const string Level2 = "Level2";
-        public const string Level3 = "Level3";
-
     }
 }
