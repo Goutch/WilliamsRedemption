18b2b65037a234891f8dbbe346b5b28ff562e92e
Simon <le6monstre@hotmail.com>
2018-11-28 15:08:13 -0500

    Ajout du knock back player jean

--- Assets/Scripts/Game/Entity/Enemies/Boss/Jean/ShieldRecuperation.cs
+++ Assets/Scripts/Game/Entity/Enemies/Boss/Jean/ShieldRecuperation.cs
@@ -1,5 +1,6 @@
 ﻿using Game.Entity.Player;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,10 @@ namespace Game.Entity.Enemies.Boss.Jean
     class ShieldRecuperation : Vulnerable
     {
         [SerializeField] private Vector2 endForceAddToPlayer;
+        [SerializeField] private float delayBeforeBeingActive;
 
         private ShieldManager shieldManager;
+        private bool isFinishing = false;
 
         private void Awake()
         {
@@ -24,12 +27,31 @@ namespace Game.Entity.Enemies.Boss.Jean
             base.Act();
         }
 
+        public override void Enter()
+        {
+            base.Enter();
+            isFinishing = false;
+        }
+
         public override void Finish()
         {
-            base.Finish();
+            if(!isFinishing)
+                StartCoroutine(WaitBeforeFinish());
+        }
+
+        public IEnumerator WaitBeforeFinish()
+        {
+            isFinishing = true;
+
+            float directionX = transform.rotation.y == 1 ? 1 : -1;
+
+            player.GetComponent<KinematicRigidbody2D>().AddForce(new Vector2(directionX * endForceAddToPlayer.x, endForceAddToPlayer.y));
+
             shieldManager.ShieldPercent = 1;
             shieldManager.IsShieldActive = true;
-            player.GetComponent<KinematicRigidbody2D>().AddForce(endForceAddToPlayer);
+
+            yield return new WaitForSeconds(delayBeforeBeingActive);
+            base.Finish();
         }
 
         public override bool CanEnter()
