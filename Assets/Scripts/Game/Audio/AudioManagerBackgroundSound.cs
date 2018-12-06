664d8973e1ee4c05ac78ef35713794c76ae7927e
FlyJonat <flyjonat@live.fr>
2018-12-06 09:19:23 -0500

    music in level1

--- Assets/Scripts/Game/Audio/AudioManagerBackgroundSound.cs
+++ Assets/Scripts/Game/Audio/AudioManagerBackgroundSound.cs
@@ -69,6 +69,7 @@ namespace Game.Audio
                 if (clip != null)
                 {
                     Init(clip);
+                    PlaySound();
                 }
             }
         }
